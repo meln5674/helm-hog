f3a9058d32bf2dd0a0b125d0a5da6972a3ead20c
Andrew Melnick <meln5674@kettering.edu>
2023-04-14 17:34:39 -0600

    Add parallel test execution, option to always keep reports

--- cmd/test.go
+++ cmd/test.go
@@ -7,6 +7,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/meln5674/helm-hog/pkg/helmhog"
 	"github.com/pkg/errors"
@@ -14,8 +15,10 @@ import (
 )
 
 var (
-	testBatch    bool
-	testOnlyLint bool
+	testBatch       bool
+	testOnlyLint    bool
+	testParallel    int
+	testKeepReports bool
 )
 
 // testCmd represents the test command
@@ -28,12 +31,16 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		cases := make(chan helmhog.Case)
 
-		if !testBatch {
-			defer os.RemoveAll(loadedProject.TempDir)
-		}
+		defer func() {
+			if testKeepReports || (testBatch && err == nil) {
+				fmt.Printf("Reports are found at %s , user is responsible for deleting this directory\n", loadedProject.TempDir)
+				return
+			}
+			os.RemoveAll(loadedProject.TempDir)
+		}()
 
 		defer func() {
 			for range cases {
@@ -44,24 +51,57 @@ to quickly create a Cobra application.`,
 
 		failed := make([]helmhog.Case, 0)
 
-		type foo struct{}
+		type result struct {
+			err error
+			c   helmhog.Case
+		}
 
-		for c := range cases {
-			err := loadedProject.MakeCaseTempDir(c)
-			if err != nil {
-				return errors.Wrap(err, fmt.Sprintf("create temp dir for case %v", c))
+		results := make(chan result)
+
+		if testParallel == 0 {
+			testParallel = runtime.NumCPU()
+		}
+		workerSem := make(chan struct{}, testParallel)
+
+		worker := func() {
+			defer func() { workerSem <- struct{}{} }()
+			for c := range cases {
+				err := func() error {
+					err := loadedProject.MakeCaseTempDir(c)
+					if err != nil {
+						return errors.Wrap(err, fmt.Sprintf("create temp dir for case %v", c))
+					}
+					if testOnlyLint {
+						err = loadedProject.Lint(c).Run()
+					} else {
+						err = loadedProject.Validate(c).Run()
+					}
+					if err == nil {
+						return err
+					}
+					writeErr := os.WriteFile(loadedProject.TempPath(c, "err"), []byte(err.Error()), 0600)
+					if writeErr != nil {
+						return errors.Wrap(err, fmt.Sprintf("write error file for case %v: %v", c, err))
+					}
+					return err
+				}()
+				results <- result{c: c, err: err}
 			}
-			if testOnlyLint {
-				err = loadedProject.Lint(c).Run()
-			} else {
-				err = loadedProject.Validate(c).Run()
+		}
+
+		for i := 0; i < testParallel; i++ {
+			go worker()
+		}
+		go func() {
+			for i := 0; i < testParallel; i++ {
+				<-workerSem
 			}
-			if err != nil {
-				failed = append(failed, c)
-				err = os.WriteFile(loadedProject.TempPath(c, "err"), []byte(err.Error()), 0600)
-				if err != nil {
-					return errors.Wrap(err, fmt.Sprintf("write error file for case %v", c))
-				}
+			close(results)
+		}()
+
+		for result := range results {
+			if result.err != nil {
+				failed = append(failed, result.c)
 			}
 		}
 
@@ -72,13 +112,13 @@ to quickly create a Cobra application.`,
 
 		fmt.Println("The following cases failed:")
 		for _, c := range failed {
-			fmt.Println(c)
+			fmt.Println(loadedProject.TempPath(c))
 		}
 		if testBatch {
-			fmt.Printf("Reports are found at %s, user is responsible for deleting this directory\n", loadedProject.TempDir)
-			return fmt.Errorf("Some tests failed!")
+			err = fmt.Errorf("Some tests failed!")
+			return
 		}
-		fmt.Printf("Reports are found at %s, press enter when ready to remove (pass --batch to not delete report directories)\n", loadedProject.TempDir)
+		fmt.Printf("Reports are found at %s, press enter when ready to remove (use --keep-reports to not delete report directories. Use --batch to skip this prompt)\n", loadedProject.TempDir)
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		return nil
@@ -88,6 +128,8 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(testCmd)
 
-	testCmd.Flags().BoolVar(&testBatch, "batch", false, "If set, do not prompt the user for report cleanup, and return non-zero on failure.")
+	testCmd.Flags().BoolVar(&testBatch, "batch", false, "If set, do not prompt the user for report cleanup, and return non-zero on failure")
 	testCmd.Flags().BoolVar(&testOnlyLint, "only-lint", false, "If set, do not attempt to do a kubectl apply --dry-run")
+	testCmd.Flags().IntVar(&testParallel, "parallel", 1, "Number of cases to run in parallel. Set to zero to use number of cpu cores")
+	testCmd.Flags().BoolVar(&testKeepReports, "keep-reports", false, "Do not delete reports, even if all cases pass")
 }
